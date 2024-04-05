8e4c29b0cf1324b33fed87c06f1c17564ae1a76f
Md. Armaan-Ul-Islam <armaan.islam@visena.com>
2024-04-06 00:51:49 +0600

    Mvn package and tomcat setup

--- src/main/java/com/aui/EcoMoneyTransferApplication.java
+++ src/main/java/com/aui/EcoMoneyTransferApplication.java
@@ -1,7 +1,22 @@
 package com.aui;
 
-public class EcoMoneyTransferApplication {
-    public static void main(String[] args) {
+import com.aui.web.TransactionServlet;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.Wrapper;
+import org.apache.catalina.startup.Tomcat;
 
+public class EcoMoneyTransferApplication {
+    public static void main(String[] args) throws LifecycleException {
+        Tomcat tomcat = new Tomcat();
+        tomcat.getConnector();
+        tomcat.setPort(8090); //todo: fix
+
+        Context context = tomcat.addContext("", null);
+        Wrapper servletWrapper = Tomcat.addServlet(context,"transactionServlet", new TransactionServlet());
+        servletWrapper.setLoadOnStartup(1);
+        servletWrapper.addMapping("/*");
+
+        tomcat.start();
     }
 }
